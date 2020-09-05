2007a295f7afa13be0f496608e48d24032c0808b
Michalkolos <cool.kolos@gmail.com>
2020-09-05 02:46:23 +0200

    Solved OpenCV C++ libraries loading with reflection that works in JDK 14.

--- src/main/java/foury/MainApp.java
+++ src/main/java/foury/MainApp.java
@@ -7,6 +7,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.opencv.core.Core;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
 
 public class MainApp extends Application {
 
@@ -19,7 +26,7 @@ public class MainApp extends Application {
 
 		scene.getStylesheets().add(getClass().getResource("gui/mainSceneStyle.css").toExternalForm());
 
-		stage.setTitle("Foury");
+		stage.setTitle("miniDFT");
 		stage.setScene(scene);
 		stage.setMaximized(true);
 		stage.show();
@@ -27,10 +34,28 @@ public class MainApp extends Application {
 
 	public static void main(String[] args) {
 
+		String currentDirectory = System.getProperty("user.dir");
+		System.out.println("The current working directory is " + currentDirectory);
+		System.out.println("Running on " + System.getProperty("os.name"));
+
+		File libraryFile = new File("libopencv_java430.so");
+		
+
+
+		String absPath = libraryFile.getAbsolutePath();
+		absPath = absPath.substring(0, absPath.length() - 20);
+
+		try {
+			addDirJava14(absPath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 		// load the native OpenCV library
-		nu.pattern.OpenCV.loadShared();
-//		nu.pattern.OpenCV.loadLocally();
-//		nu.pattern.OpenCV.loadLibrary();
+//		nu.pattern.OpenCV.loadShared();
+		nu.pattern.OpenCV.loadLocally();
+
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 //		System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
 
@@ -38,5 +63,66 @@ public class MainApp extends Application {
 	}
 
 
+
+	public static void addDirJava14(String s) throws IOException {
+		MethodHandles.Lookup cl = null;
+
+		try {
+			cl = MethodHandles.privateLookupIn(ClassLoader.class, MethodHandles.lookup());
+		} catch (IllegalAccessException e) {
+			throw new IOException("Failed to create MethodHandle");
+		}
+
+
+		VarHandle usr_paths = null;
+
+		try {
+			usr_paths = cl.findStaticVarHandle(ClassLoader.class, "usr_paths", String[].class);
+		} catch (NoSuchFieldException e) {
+			throw new IOException("Cannot find usr_paths field");
+		} catch (IllegalAccessException e) {
+			throw new IOException("Access to usr_paths field was denied");
+		}
+
+		//		usr_paths.set(null);
+
+		String[] pathStrings = (String[])usr_paths.get();
+		String[] newPathStrings = new String[pathStrings.length + 1];
+		System.arraycopy(pathStrings, 0, newPathStrings, 0, pathStrings.length);
+		newPathStrings[pathStrings.length] = s;
+
+		usr_paths.set(newPathStrings);
+
+		System.out.println("Usr paths: " +  Arrays.toString((String[])usr_paths.get()));
+
+	}
+
+
+	public static void addDir(String s) throws IOException {
+		try {
+			// This enables the java.library.path to be modified at runtime
+			// From a Sun engineer at http://forums.sun.com/thread.jspa?threadID=707176
+			//
+			Field field = ClassLoader.class.getDeclaredField("usr_paths");
+			field.setAccessible(true);
+			String[] paths = (String[])field.get(null);
+			for (int i = 0; i < paths.length; i++) {
+				if (s.equals(paths[i])) {
+					return;
+				}
+			}
+			String[] tmp = new String[paths.length+1];
+			System.arraycopy(paths,0,tmp,0,paths.length);
+			tmp[paths.length] = s;
+			field.set(null,tmp);
+			System.setProperty("java.library.path", System.getProperty("java.library.path") + File.pathSeparator + s);
+		} catch (IllegalAccessException e) {
+			throw new IOException("Failed to get permissions to set library path");
+		} catch (NoSuchFieldException e) {
+			throw new IOException("Failed to get field handle to set library path");
+		}
+	}
 }
+
+
 
